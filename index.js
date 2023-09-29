6a76d726bad20989cdf9ea07dae619ad496fe169
Jose Valero <jvalero000@gmail.com>
2023-09-29 16:20:54 -0700

    Added more styling and functionality.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
@@ -8,19 +8,36 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs", {
+    items: items,
+  });
 });
 
 app.post("/", (req, res) => {
-    items.push(req.body["item"]);
-    console.log(items);
-    res.render("index.ejs", {
-        items: items
-    });
+  items.push(req.body["item"]);
+  console.log(items);
+  res.render("index.ejs", {
+    items: items,
+  });
+});
+
+app.get("/work", (req, res) => {
+  res.render("work.ejs", {
+    itemsWork: itemsWork,
+  });
+});
+
+app.post("/work", (req, res) => {
+  itemsWork.push(req.body["item"]);
+  console.log(itemsWork);
+  res.render("work.ejs", {
+    itemsWork: itemsWork,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 let items = [];
+let itemsWork = [];
